8a0c7b270ca11ad2218d4bc1ace6d0feaf62d292
Raphael Bottino <raphabot@gmail.com>
2023-03-23 15:32:43 -0500

    Updating messaging.

--- pull-scan.sh
+++ pull-scan.sh
@@ -12,12 +12,25 @@ c1cs scan -e "artifactscan.$REGION.cloudone.trendmicro.com" docker-archive:"$IMA
 cat "$SCAN_RESULT_ARTIFACT"
 
 # Evaluates the result
- if [ "$(jq '.totalVulnCount' "$SCAN_RESULT_ARTIFACT")" -ge "$MAX_TOTAL" ]; then exit 1; fi
- if [ "$(jq '.criticalCount' "$SCAN_RESULT_ARTIFACT")" -ge "$MAX_CRITICAL" ]; then exit 1; fi
- if [ "$(jq '.highCount' "$SCAN_RESULT_ARTIFACT")" -ge "$MAX_HIGH" ]; then exit 1; fi
- if [ "$(jq '.mediumCount' "$SCAN_RESULT_ARTIFACT")" -ge "$MAX_MEDIUM" ]; then exit 1; fi
- if [ "$(jq '.lowCount' "$SCAN_RESULT_ARTIFACT")" -ge "$MAX_LOW" ]; then exit 1; fi
- if [ "$(jq '.negligibleCount' "$SCAN_RESULT_ARTIFACT")" -ge "$MAX_NEGLIGIBLE" ]; then exit 1; fi
- if [ "$(jq '.unknownCount' "$SCAN_RESULT_ARTIFACT")" -ge "$MAX_UNKNOWN" ]; then exit 1; fi
+MESSAGE=""
+TOTAL_VULNERABILITIES="$(jq '.totalVulnCount' "$SCAN_RESULT_ARTIFACT")"
+if [ "$TOTAL_VULNERABILITIES" -ge "$MAX_TOTAL" ]; then MESSAGE+="Your total vulnerabilities is $TOTAL_VULNERABILITIES, which is higher than $MAX_TOTAL\n"; fi
+TOTAL_CRITICAL="$(jq '.criticalCount' "$SCAN_RESULT_ARTIFACT")"
+if [ "$TOTAL_CRITICAL" -ge "$TOTAL_CRITICAL" ]; then MESSAGE+="Your total of Critical vulnerabilities is $TOTAL_CRITICAL, which is higher than $MAX_CRITICAL\n"; fi
+TOTAL_HIGH="$(jq '.highCount' "$SCAN_RESULT_ARTIFACT")"
+if [ "$TOTAL_HIGH" -ge "$MAX_HIGH" ]; then MESSAGE+="Your total of High vulnerabilities is $TOTAL_HIGH, which is higher than $MAX_HIGH\n"; fi
+TOTAL_MEDIUM="$(jq '.mediumCount' "$SCAN_RESULT_ARTIFACT")"
+if [ "$TOTAL_MEDIUM" -ge "$MAX_MEDIUM" ]; then MESSAGE+="Your total of Medium vulnerabilities is $TOTAL_MEDIUM, which is higher than $MAX_MEDIUM\n"; fi
+TOTAL_LOW="$(jq '.lowCount' "$SCAN_RESULT_ARTIFACT")"
+if [ "$TOTAL_LOW" -ge "$MAX_LOW" ]; then MESSAGE+="Your total of Low vulnerabilities is $TOTAL_LOW, which is higher than $MAX_LOW\n"; fi
+TOTAL_NEGLIGIBLE="$(jq '.negligibleCount' "$SCAN_RESULT_ARTIFACT")"
+if [ "$TOTAL_NEGLIGIBLE" -ge "$MAX_NEGLIGIBLE" ]; then MESSAGE+="Your total of Negligible vulnerabilities is $TOTAL_NEGLIGIBLE, which is higher than $MAX_NEGLIGIBLE\n"; fi
+TOTAL_UNKNOWN="$(jq '.unknownCount' "$SCAN_RESULT_ARTIFACT")"
+if [ "$TOTAL_UNKNOWN" -ge "$MAX_UNKNOWN" ]; then MESSAGE+="Your total of Unknown vulnerabilities is $TOTAL_UNKNOWN, which is higher than $MAX_UNKNOWN\n"; fi
 
- exit 0
+# Issue found
+if [ "$MESSAGE" = "" ]; then echo "$MESSAGE"; exit 1; fi
+
+# No issues found
+echo "Evaluation found no issues against the provided policy."
+exit 0
