7770ad120a4866b5ca4b61b0a58185a35aab58f5
Raphael Bottino <raphabot@gmail.com>
2023-03-24 14:15:26 -0500

    Fixing print line. (#2)
    * Adding result evaluation.
    
    * Updating messaging.
    
    * removing test file.
    
    * Replacing echo with printf
    
    * fixing errors
    
    * removing last \n
    
    * SC2059
    
    * fixing if

--- pull-scan.sh
+++ pull-scan.sh
@@ -14,7 +14,7 @@ cat "$SCAN_RESULT_ARTIFACT"
 # Evaluates the result
 MESSAGE=""
 TOTAL_VULNERABILITIES="$(jq '.totalVulnCount' "$SCAN_RESULT_ARTIFACT")"
-if [ "$TOTAL_VULNERABILITIES" -gt "$MAX_TOTAL" ]; then MESSAGE+="Your total vulnerabilities is $TOTAL_VULNERABILITIES, which is higher than $MAX_TOTAL"; fi
+if [ "$TOTAL_VULNERABILITIES" -gt "$MAX_TOTAL" ]; then MESSAGE+="Your total vulnerabilities is $TOTAL_VULNERABILITIES, which is higher than $MAX_TOTAL\n"; fi
 TOTAL_CRITICAL="$(jq '.criticalCount' "$SCAN_RESULT_ARTIFACT")"
 if [ "$TOTAL_CRITICAL" -gt "$MAX_CRITICAL" ]; then MESSAGE+="Your total of Critical vulnerabilities is $TOTAL_CRITICAL, which is higher than $MAX_CRITICAL\n"; fi
 TOTAL_HIGH="$(jq '.highCount' "$SCAN_RESULT_ARTIFACT")"
@@ -26,10 +26,10 @@ if [ "$TOTAL_LOW" -gt "$MAX_LOW" ]; then MESSAGE+="Your total of Low vulnerabili
 TOTAL_NEGLIGIBLE="$(jq '.negligibleCount' "$SCAN_RESULT_ARTIFACT")"
 if [ "$TOTAL_NEGLIGIBLE" -gt "$MAX_NEGLIGIBLE" ]; then MESSAGE+="Your total of Negligible vulnerabilities is $TOTAL_NEGLIGIBLE, which is higher than $MAX_NEGLIGIBLE\n"; fi
 TOTAL_UNKNOWN="$(jq '.unknownCount' "$SCAN_RESULT_ARTIFACT")"
-if [ "$TOTAL_UNKNOWN" -gt "$MAX_UNKNOWN" ]; then MESSAGE+="Your total of Unknown vulnerabilities is $TOTAL_UNKNOWN, which is higher than $MAX_UNKNOWN\n"; fi
+if [ "$TOTAL_UNKNOWN" -gt "$MAX_UNKNOWN" ]; then MESSAGE+="Your total of Unknown vulnerabilities is $TOTAL_UNKNOWN, which is higher than $MAX_UNKNOWN"; fi
 
 # Issue found
-if [ "$MESSAGE" != "" ]; then echo "$MESSAGE"; exit 1; fi
+if [ "$MESSAGE" != "" ]; then printf "%s" "$MESSAGE"; exit 1; fi
 
 # No issues found
 echo "Evaluation found no issues against the provided policy."
